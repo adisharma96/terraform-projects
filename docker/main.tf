7b4ef012e361d5c023944bb968c9170a981b8686
adisharma96 <adisharma96@gmail.com>
2023-05-29 17:20:29 +0530

    update port mappings

--- docker/main.tf
+++ docker/main.tf
@@ -14,8 +14,8 @@ resource "docker_container" "web_server" {
     image = docker_image.nginx.image_id
     name = "webserver"
     ports {
-      internal = "80"
-      external = "3000"
+      internal = "3000"
+      external = "80"
     }
 
     ports {
