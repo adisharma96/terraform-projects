4b32a63748edb52b2e0c47640041dabe17fa3ddb
adisharma96 <adisharma96@gmail.com>
2023-05-29 17:17:10 +0530

    docker via terraform

---
+++ docker/main.tf
@@ -0,0 +1,25 @@
+provider "docker" {
+  registry_auth {
+    address  = "docker-registry.io"
+    username = "$DOCKER_REGISTRY_USER"
+    password = "$DOCKER_REGISTRY_PASS"
+  }
+}
+resource "docker_image" "nginx" {
+    name = "adisharma96/weather-app:jenkins"
+
+}
+
+resource "docker_container" "web_server" {
+    image = docker_image.nginx.image_id
+    name = "webserver"
+    ports {
+      internal = "80"
+      external = "3000"
+    }
+
+    ports {
+       internal = "8080"
+       external = "5000"
+    }
+}
