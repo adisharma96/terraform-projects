e24c0221f244e13a0b80aa0f97c997fae4e5b99a
adisharma96 <adisharma96@gmail.com>
2023-08-16 18:34:27 +0530

    updated vpc code

--- vpc/main.tf
+++ vpc/main.tf
@@ -6,6 +6,10 @@ resource "aws_vpc" "my_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
+  tags = {
+     "Name" = "terraform-vpc"
+
+  }
 
 }
 
